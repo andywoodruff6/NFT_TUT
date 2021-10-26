07671ad79be4f28380fa57f82a2d347d375f6702
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2021-10-26 10:18:35 -0400

    refactored

--- Code_Build/deploy/01_deploy_svgnft.js
+++ Code_Build/deploy/01_deploy_svgnft.js
@@ -10,24 +10,24 @@ module.exports = async ({
     const {deployer} = await getNamedAccounts();
     const chainId = await getChainId();
 
-    log("------------------------------")
-    const SVGNFT = await deploy("SVGNFT", { 
-        from: deployer, 
-        log: true }
-    );
+    log("------------------------------");
+    const SVGNFT = await deploy("SVGNFT", { from: deployer, log: true });
+
     log(`You have deployed an NFT contract to ${SVGNFT.address}`);
-    let filePath ="./img/half-hexagon-fun.svg"
+
+    let filePath ="./img/half-hexagon-fun.svg";
     let svg = fs.readFileSync(filePath, {encoding: "utf8"});
 
     const svgNFTContract = await ethers.getContractFactory("SVGNFT");
     const accounts = await hre.ethers.getSigners();
-    const signer = accounts[0]
+    const signer = accounts[0];
     const svgNFT = new ethers.Contract(SVGNFT.address, svgNFTContract.interface, signer);
-    const networkName = networkConfig[chainId]['name']
-    log(`Verify with: \n npx hardhat verify --network ${networkName} ${svgNFT.address}`)
+    const networkName = networkConfig[chainId]['name'];
+
+    log(`Verify with: \n npx hardhat verify --network ${networkName} ${svgNFT.address}`);
 
     let transactionResponse = await svgNFT.create(svg);
-    let receipt = await transactionResponse.wait(1);
+    await transactionResponse.wait(1);
     log(`You have created an NFT`);
-    log(`You can view the tokenURI here ${await svgNFT.tokenURI(0)}`)
+    log(`You can view the tokenURI here ${await svgNFT.tokenURI(0)}`);
 }
